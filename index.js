533731759451b81d65560a51d294ae5518977fad
Rtznn7 <joaocriscm274720@gmail.com>
2025-12-12 17:09:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const sons = {
+    'A': '1.mp3.',
+    'S': '2.mp3 ',
+    'D': '3.mp3',
+    'F': '4.mp3',
+    'G': '5.mp3',
+    'H': '6.mp3',
+    'J': '7.mp3',
+    'K': '8.mp3',
+    'L': '9.mp3',
+}
+
+const criarDiv = (texto) => {
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.textContent = texto;
+    div.id = texto;
+    document.getElementById('container').appendChild(div);
+}
+
+const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
+
+const tocarSom = (letra) => {
+    const audio = new Audio(`./sounds/${sons[letra]}`);
+    audio.play();
+}
+
+const adicionarEfeito = (letra) => document.getElementById(letra)
+                                           .classList.toggle('active');
+
+const removerEfeito = (letra) => {
+    const div = document.getElementById(letra);
+    const removeActive = () => div.classList.remove('active');
+    div.addEventListener('transitionend',removeActive);
+};
+
+const ativarDiv = (evento) => {
+
+    const letra = evento.type == 'click' ? evento.target.id : evento.key.toUpperCase();
+    
+    const letraPermitida = sons.hasOwnProperty(letra);
+    if (letraPermitida){
+        adicionarEfeito(letra);
+        tocarSom(letra);
+        removerEfeito(letra);
+    }
+}
+
+
+exibir(sons);
+document.getElementById('container')
+        .addEventListener('click', ativarDiv);
+
+window.addEventListener('keyup',ativarDiv);
